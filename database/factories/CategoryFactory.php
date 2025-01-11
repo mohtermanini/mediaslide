60883a24d3a583e1ac0159878d2d7f15d9287b84
Mohamad Termanini <mohtermanini.job@gmail.com>
2025-01-11 15:02:16 +0400

    Update Fake Name Generating

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->regexify('[A-Za-z0-9]{10}'),
+            'name' => $this->faker->word(),
         ];
     }
 }
